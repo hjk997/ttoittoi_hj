7b4e13ae767604a749eaa8b41fa84907bdd4240d
hjk997 <ununsk@gmail.com>
2022-07-31 03:37:11 +0900

    가로, 세로 사이즈 설정 버튼 생성 가로세로 사이즈를 설정하고 시작 버튼을 누르면 보드판이 만들어진다. 가로, 세로 사이즈의 최소, 최대 범위 지정

--- minesweeper/start.js
+++ minesweeper/start.js
@@ -3,24 +3,50 @@ window.onload = function(){
   let help = document.getElementById('test');
   console.log("help");
 
+};
+
+function initTable(){
+
+  let rowSize = document.getElementById("rowSize").value;
+  let colSize = document.getElementById("colSize").value;
   let table = document.getElementById('gameBoard');
 
-  // 지뢰가 할당될 임의 좌표를 5개 지정한다.
+  if(table.hasChildNodes()){
+    console.log(table.childElementCount);
+    while(table.hasChildNodes()){
+      table.removeChild(table.firstChild);
+    }
+  }
 
-  for(let i = 0 ; i < 8 ; i++){
+  if(rowSize < 5 || rowSize > 50){
+    alert("가로 사이즈가 너무 큽니다!");
+    return;
+  }
+
+  if(colSize < 5 || colSize > 50){
+    alert("세로 사이즈가 너무 큽니다!");
+    return;
+  }
+
+  for(let i = 0 ; i < rowSize ; i++){
     // 1. 행을 만든다
     // 2. 열을 만든다
     // 3. 열 안에 이벤트 등록된 버튼을 넣는다
     let row = document.createElement('tr');
-    for(let j = 0 ; j < 8 ; j++){
-      col = document.createElement('td');
+    row.setAttribute("id", "tr_" + i);
+    for(let j = 0 ; j < colSize ; j++){
+      let col = document.createElement('td');
       col.type = 'td';
-      col.innerHTML = '<button id="btn_"' + i + '_' + j + ' onclick="pushButton(this)"><img src="./img/carrot.png" height="50" width="50"></button>';
-      col.className = 'td_' + i + '_' + j;
+      col.innerHTML = '<button id="btn_"' + j + '_' + i + ' onclick="pushButton(this)"><img src="./img/carrot.png" height="50" width="50"></button>';
+      col.className = 'td_' + j + '_' + i;
 
       row.append(col);
     }
 
     table.append(row);
   }
-};
+}
+
+function pushButton(btn){
+
+}
